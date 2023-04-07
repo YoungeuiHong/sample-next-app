1aabd5ffc56e513e022a14f1603eff34b4285116
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2023-04-07 20:30:59 +0900

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@sample-next-app/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
